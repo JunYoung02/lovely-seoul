9e843fd2b70b4d49b36916695cd06a0c8c8c4038
JunYoung <whwnsdud011@naver.com>
2024-10-06 23:19:36 +0900

    fix: console.log 주석처리

--- src/components/common/card/Card.jsx
+++ src/components/common/card/Card.jsx
@@ -3,7 +3,7 @@ import CardItem from './CardItem';
 // import { placeData } from '../../../utils/PlaceRtd';
 
 function Card({ props }) {
-  console.log(props);
+  // console.log(props);
   return (
     <div className={styles.listwrapper}>
       {props.map((place) => (
